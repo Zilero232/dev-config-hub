5ba6562aafdac6232e45ef43c85a07ad113db546
Alexandr <sasha.artemev.1002@mail.ru>
2024-07-07 14:02:29 +0300

    :recycle: set up husky and commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,19 +12,11 @@ const commitTypes = {
   'chore': ':wrench:'
 };
 
-const commitRegex = /^(\w+)(?=:)/; // Регулярное выражение для извлечения типа коммита
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   parserPreset: 'conventional-changelog-atom',
   formatter: '@commitlint/format',
   rules: {
-    'type-enum': [2, 'always', Object.keys(commitTypes)],
-    'format': (commit) => {
-      const [, type] = commit.match(commitRegex) || []; // Деструктуризация и извлечение типа коммита
-      const emoji = commitTypes[type];                  // Получение соответствующего смайлика
-
-      return emoji ? `${emoji} ${commit}` : commit;     // Форматирование сообщения с смайликом или без него
-    }
+    'type-enum': [2, 'always', Object.keys(commitTypes)]
   }
 };
